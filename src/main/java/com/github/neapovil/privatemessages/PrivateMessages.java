11d6452f31db116aa5511940e17d58debf3063d3
Nearata <nearata@protonmail.com>
2022-05-10 21:06:59 +0200

    MiniMessage and editable messages

--- src/main/java/com/github/neapovil/privatemessages/PrivateMessages.java
+++ src/main/java/com/github/neapovil/privatemessages/PrivateMessages.java
@@ -1,17 +1,35 @@
 package com.github.neapovil.privatemessages;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.electronwill.nightconfig.core.file.FileConfig;
 import com.github.neapovil.privatemessages.command.MessageCommand;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
 public final class PrivateMessages extends JavaPlugin
 {
     private static PrivateMessages instance;
+    private FileConfig messages;
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     @Override
     public void onEnable()
     {
         instance = this;
+
+        this.saveResource("messages.json", false);
+
+        this.messages = FileConfig.builder(new File(this.getDataFolder(), "messages.json"))
+                .autoreload()
+                .autosave()
+                .build();
+
+        this.messages.load();
 
         MessageCommand.register();
     }
@@ -24,5 +42,11 @@ public final class PrivateMessages extends JavaPlugin
     public static PrivateMessages getInstance()
     {
         return instance;
+    }
+
+    public Component getMessageComponent(String path, String playerName, String content)
+    {
+        final String message = this.messages.get("messages." + path);
+        return this.miniMessage.deserialize(message, Placeholder.parsed("player_name", playerName), Placeholder.parsed("content", content));
     }
 }
--- src/main/java/com/github/neapovil/privatemessages/command/MessageCommand.java
+++ src/main/java/com/github/neapovil/privatemessages/command/MessageCommand.java
@@ -2,25 +2,28 @@ package com.github.neapovil.privatemessages.command;
 
 import org.bukkit.entity.Player;
 
+import com.github.neapovil.privatemessages.PrivateMessages;
+
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.GreedyStringArgument;
-import dev.jorel.commandapi.arguments.PlayerArgument;
-import net.md_5.bungee.api.ChatColor;
 
 public final class MessageCommand
 {
+    private static final PrivateMessages plugin = PrivateMessages.getInstance();
+
     public static final void register()
     {
         new CommandAPICommand("message")
                 .withPermission("privatemessages.command.message")
-                .withArguments(new PlayerArgument("player"))
+                .withArguments(new EntitySelectorArgument("player", EntitySelectorArgument.EntitySelector.ONE_PLAYER))
                 .withArguments(new GreedyStringArgument("content"))
                 .executes((sender, args) -> {
                     final Player player = (Player) args[0];
                     final String content = (String) args[1];
 
-                    sender.sendMessage(ChatColor.GRAY + "[me -> " + player.getName() + "]: " + ChatColor.RESET + content);
-                    player.sendMessage(ChatColor.GREEN + "[" + sender.getName() + " -> me]: " + ChatColor.RESET + content);
+                    sender.sendMessage(plugin.getMessageComponent("sender", player.getName(), content));
+                    player.sendMessage(plugin.getMessageComponent("receiver", sender.getName(), content));
                 })
                 .register();
     }
